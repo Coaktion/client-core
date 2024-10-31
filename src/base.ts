42db061a690346dc239a62dc60dbacc7ef33b17d
Paulo Tinoco <paulo.oliveira@seedz.ag>
2024-10-31 14:41:45 -0300

    fix: improve error handling in retryDelay method to support optional chaining

--- src/base.ts
+++ src/base.ts
@@ -294,8 +294,8 @@ export class BaseClient implements BaseClientInterface {
    */
   retryDelay = (retryCount: number, error: AxiosError | any): number => {
     if (retryCount >= this.clientOptions.tries)
-      throw error.response ? error.response : error;
-    return error.response.status ===
+      throw error?.response ? error.response : error;
+    return error?.response?.status ===
       HttpStatusCodesRetryCondition.TooManyRequests
       ? parseInt(error.response.headers[this.clientOptions.rateLimitKey])
       : this.clientOptions.retryDelay * 1000;
